752d845af8ab9d2acd7f7d393de085965bb1625d
whutsup86 <33618387+whutsup86@users.noreply.github.com>
2024-08-26 18:55:44 +0900

    Add files via upload

---
+++ js/onmouse.js
@@ -0,0 +1,55 @@
+
+function AR_start() {
+    document.getElementById('AR_image').style.opacity = "1";
+}
+
+function AR_stop() {
+    document.getElementById('AR_image').style.opacity = "0";
+}
+AR_stop()
+
+function Qtracker_start() {
+    document.getElementById('Qtracker_image').style.opacity = "1";
+    
+}
+
+function Qtracker_stop() {
+    document.getElementById('Qtracker_image').style.opacity = "0";
+}
+Qtracker_stop()
+
+function LUNE_start() {
+    document.getElementById('LUNE_image').style.opacity = "1";
+}
+
+function LUNE_stop() {
+    document.getElementById('LUNE_image').style.opacity = "0";
+}
+LUNE_stop()
+
+function Hug2Go_start() {
+    document.getElementById('Hug2Go_image').style.opacity = "1";
+}
+
+function Hug2Go_stop() {
+    document.getElementById('Hug2Go_image').style.opacity = "0";
+}
+Hug2Go_stop()
+
+function SafeT_start() {
+    document.getElementById('SafeT_image').style.opacity = "1";
+}
+
+function SafeT_stop() {
+    document.getElementById('SafeT_image').style.opacity = "0";
+}
+SafeT_stop()
+
+function Tracker_start() {
+    document.getElementById('Tracker_image').style.opacity = "1";
+}
+
+function Tracker_stop() {
+    document.getElementById('Tracker_image').style.opacity = "0";
+}
+Tracker_stop()
